404525d10805628c1a1a1cbadce2017bd84be83b
teri.epi <teri.epi@kakaoenterprise.com>
2021-12-14 20:56:08 +0900

    spring-data jpa 테스트 코드

---
+++ src/main/java/kr/pe/acet/controller/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package kr.pe.acet.controller.web.dto;
+
+import kr.pe.acet.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
