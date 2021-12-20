f8e89c31139c98f1e3cb6d3a0848fa3258292bc9
teri.epi <teri.epi@kakaoenterprise.com>
2021-12-20 20:47:59 +0900

    ui 머시테치 사용 및 게시글 등록 / 조회 기능 추가

---
+++ src/main/java/kr/pe/acet/controller/web/IndexController.java
@@ -0,0 +1,28 @@
+package kr.pe.acet.controller.web;
+
+import kr.pe.acet.controller.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+}
