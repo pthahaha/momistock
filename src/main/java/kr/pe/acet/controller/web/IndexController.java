6741472edbc203ac88a2aaa355cf5f13272d8fd7
teri.epi <teri.epi@kakaoenterprise.com>
2022-01-02 19:54:36 +0900

    ui 머시테치 사용 및 게시글 수정 / 삭제

--- src/main/java/kr/pe/acet/controller/web/IndexController.java
+++ src/main/java/kr/pe/acet/controller/web/IndexController.java
@@ -1,10 +1,15 @@
 package kr.pe.acet.controller.web;
 
 import kr.pe.acet.controller.service.PostsService;
+import kr.pe.acet.controller.web.dto.PostsResponseDto;
+import kr.pe.acet.controller.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RequiredArgsConstructor
@@ -23,6 +28,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
