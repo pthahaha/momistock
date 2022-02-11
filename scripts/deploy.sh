0ba34ab5b530d08d441a40fed3eec5bb7ad89107
teri.epi <teri.epi@kakaoenterprise.com>
2022-02-11 16:56:34 +0900

    build된 jar에 실행 권한 주기 및 실행 해보기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,16 +33,10 @@ fi
 echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
 echo "> JAR Name: $JAR_NAME"
 
-#echo "> $JAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
 
-#chmod 700 $JAR_NAME
-#
-#echo "> $JAR_NAME 실행"
-#
-#nohup java -jar \
-#    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/service/application-oauth.properties,/home/ec2-user/service/application-real-db.properties \
-#    -Dspring.profiles.active=real \
-#    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_NAME 실행"
+nohup java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
