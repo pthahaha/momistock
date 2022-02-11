d4a7250eccbf76fd4640ac600860b7897aad0454
teri.epi <teri.epi@kakaoenterprise.com>
2022-02-11 17:30:53 +0900

    jar -jar OAuth conf 호출부 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,4 +39,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
