14d75cfeec9858ab757ef426088fd9260704565f
teri.epi <teri.epi@kakaoenterprise.com>
2022-02-11 16:46:33 +0900

    jar path 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,31 +15,29 @@ echo "> move service dir "
 cd $REPOSITORY
 
 echo "> Build 파일 복사"
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl momistock | grep jar | awk '{print $1}')
 
-#echo "> 현재 구동중인 애플리케이션 pid 확인"
-#
-#CURRENT_PID=$(pgrep -fl momistock | grep jar | awk '{print $1}')
-#
-#echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-#
-#if [ -z "$CURRENT_PID" ]; then
-#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-#else
-#    echo "> kill -15 $CURRENT_PID"
-#    kill -15 $CURRENT_PID
-#    sleep 5
-#fi
-#
-#echo "> 새 어플리케이션 배포"
-#
-#JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-#
-#echo "> JAR Name: $JAR_NAME"
-#
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
 #echo "> $JAR_NAME 에 실행권한 추가"
-#
+
 #chmod 700 $JAR_NAME
 #
 #echo "> $JAR_NAME 실행"
