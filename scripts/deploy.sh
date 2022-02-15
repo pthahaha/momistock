35c38ccf97df42b9a7bb19d8a9a5afd536840ae0
teri.epi <teri.epi@kakaoenterprise.com>
2022-02-15 20:43:12 +0900

    real properteis classpath로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,6 +39,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
--Dspring.config.location=classpath:/application.properties,/home/ec2-user/service/application-oauth.properties,/home/ec2-user/service/application-real-db.properties,/home/ec2-user/service/application-real.properties \
+-Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/service/application-oauth.properties,/home/ec2-user/service/application-real-db.properties \
 -Dspring.profiles.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
